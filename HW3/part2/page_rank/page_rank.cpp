228bb175dc41e9118f46afd5b1b1c70b2cb96564
chuckwu2000 <chuckwu92@gmail.com>
2023-11-15 15:47:21 +0800

    [part2/PageRank done]
    * fixed bug
    - double & int shouldn't mixuse
    
    * add openmp direction

--- HW3/part2/page_rank/page_rank.cpp
+++ HW3/part2/page_rank/page_rank.cpp
@@ -17,12 +17,10 @@
 //
 void pageRank(Graph g, double *solution, double damping, double convergence)
 {
-
-  // initialize vertex weights to uniform probability. Double
-  // precision scores are used to avoid underflow for large graphs
-
   	int numNodes = num_nodes(g);
   	double equal_prob = 1.0 / numNodes;
+
+	#pragma omp parallel for
   	for (int i = 0; i < numNodes; ++i)
   	{
 		solution[i] = equal_prob;
@@ -33,12 +31,14 @@ void pageRank(Graph g, double *solution, double damping, double convergence)
 
 	while(!converged)
 	{
-		int node_no_out_edge_sum = 0;
-		for (int i = 0; i < numNodes; ++i)
+		double node_no_out_edge_sum = 0.0;
+
+		#pragma omp parallel for reduction (+:node_no_out_edge_sum)
+		for(int i = 0;i < numNodes;i++)
 		{
 			int size = outgoing_size(g, i);
 			if(size == 0)
-		  	{
+		 	{
 				node_no_out_edge_sum += solution[i];
 			}
 			else
@@ -46,35 +46,37 @@ void pageRank(Graph g, double *solution, double damping, double convergence)
 				node_out_edge_give[i] = solution[i] / size;
 			}
 		}
-		node_no_out_edge_sum = node_no_out_edge_sum * (damping / numNodes);
+		node_no_out_edge_sum = damping * node_no_out_edge_sum / numNodes;
 
-		printf("node_no_out_edge_sum : %d\n",node_no_out_edge_sum);
+		double global_diff = 0.0;
 
-		double global_diff = 0;
+		#pragma omp parallel for reduction (+:global_diff)
 		for(int i = 0;i < numNodes;i++)
 		{
 			double score_new = 0.0;
+
 			const Vertex* in_edge_begin = incoming_begin(g, i);
 			const Vertex* in_edge_end = incoming_end(g, i);
-
 			for(const Vertex *iter = in_edge_begin;iter != in_edge_end;iter++)
 			{
-				score_new = score_new + node_out_edge_give[*iter];
+				score_new += node_out_edge_give[*iter];
 			}
 
-			score_new = (damping * score_new) + (1.0-damping) / numNodes + node_no_out_edge_sum;
+			score_new = (damping * score_new) + (1.0 - damping) / numNodes + node_no_out_edge_sum;
 
-			global_diff  = global_diff + abs(solution[i] - score_new);
-			solution[i] = abs(solution[i] - score_new);
+			global_diff += abs(solution[i] - score_new);
+
+			solution[i] = score_new;
 		}
-
-		printf("global_diff : %lf , convergence : %lf\n",global_diff, convergence);
-
 		converged = (global_diff < convergence) ?1 :0;
 	}
 
 	free(node_out_edge_give);
 	return;
+
+  // initialize vertex weights to uniform probability. Double
+  // precision scores are used to avoid underflow for large graphs
+
   /*
 	 For PP students: Implement the page rank algorithm here.  You
 	 are expected to parallelize the algorithm using openMP.  Your
