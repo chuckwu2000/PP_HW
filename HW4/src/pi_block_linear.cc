84aad8bbc80e17e516c4d015c14f69676036136c
chuckwu2000 <chuckwu92@gmail.com>
2023-11-24 22:09:13 +0800

    [part1.1 part1.2.1 done]

--- HW4/src/pi_block_linear.cc
+++ HW4/src/pi_block_linear.cc
@@ -16,19 +16,46 @@ int main(int argc, char **argv)
     // ---
 
     // TODO: init MPI
+	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    long long number_of_toss = tosses;
+    long long number_in_circle = 0;
+    long long size = number_of_toss / world_size;
+    long long number_in_circle_per_process = 0;
+
+    unsigned int seed = (unsigned)time(NULL) * (unsigned)world_rank;
+    for(int i = 0; i < size; i++)
+    {
+        double x = ((double)rand_r(&seed) / RAND_MAX);
+        double y = ((double)rand_r(&seed) / RAND_MAX);
+        double distance_squared = x * x + y * y;
+        if(distance_squared <= 1)
+        {
+            number_in_circle_per_process++;
+        }
+    }
 
     if (world_rank > 0)
     {
         // TODO: handle workers
+		MPI_Send(&number_in_circle_per_process, 1, MPI_LONG_LONG, 0, 0, MPI_COMM_WORLD);
     }
     else if (world_rank == 0)
     {
         // TODO: master
+		number_in_circle += number_in_circle_per_process;
+        for(int i = 1; i < world_size; i++)
+        {
+            MPI_Recv(&number_in_circle_per_process, 1, MPI_LONG_LONG, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            number_in_circle += number_in_circle_per_process;
+        }
     }
 
     if (world_rank == 0)
     {
         // TODO: process PI result
+		pi_result = 4 * number_in_circle / ((double) number_of_toss);
 
         // --- DON'T TOUCH ---
         double end_time = MPI_Wtime();
